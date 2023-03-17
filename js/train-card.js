2843f8c4456ab79ba00aee8d91004196f477aa92
thjungers <thomasjungers@gmail.com>
2023-03-17 16:13:59 +0100

    Display stops in train cards

--- js/train-card.js
+++ js/train-card.js
@@ -1,4 +1,5 @@
 import { getVehicle } from "./api-calls.js"
+import { formatTime, tsToDate, formatDelay } from "./utils.js"
 
 /**
  * Custom component for train watch cards
@@ -20,6 +21,11 @@ export default class TrainCard extends HTMLElement {
 
         /** @type {Boolean} Whether the custom element is connected to the DOM */
         this.connected = false
+
+        /** @type {?int} The ID of the timer used to re-render the card */
+        this.renderTimer = null
+        /** @type {?int} The ID of the timer used to update the card data */
+        this.UpdateTimer = null
         
         const template = document.getElementById("train-card-template")
         /** @type {ShadowRoot} The root shadow element */
@@ -27,11 +33,11 @@ export default class TrainCard extends HTMLElement {
         this.shadow.appendChild(template.content.cloneNode(true))
         this.shadow.appendChild(document.getElementById("bootstrap-css").cloneNode())
         this.shadow.appendChild(document.getElementById("bootstrap-icons-css").cloneNode())
-
-        /** @type {?int} The ID of the timer used to re-render the card */
-        this.renderTimer = null
-        /** @type {?int} The ID of the timer used to update the card data */
-        this.UpdateTimer = null
+        
+        const dedicatedCSS = document.createElement("link")
+        dedicatedCSS.setAttribute("rel", "stylesheet")
+        dedicatedCSS.setAttribute("href", "css/train-card.css")
+        this.shadow.appendChild(dedicatedCSS)
     }
 
     static get observedAttributes() { return ["train-number", "station"] }
@@ -58,9 +64,32 @@ export default class TrainCard extends HTMLElement {
         }
     }
 
+    /**
+     * Show an error box instead of the stops table
+     * @param {Boolean} show Whether to show (true) or hide (false) the error box
+     */
+    showErrorBox(show) {
+        if(show) {
+            this.shadow.querySelector(".tc-error").classList.remove("d-none")
+            this.shadow.querySelector(".tc-table").classList.add("d-none")
+        }
+        else {
+            this.shadow.querySelector(".tc-error").classList.add("d-none")
+            this.shadow.querySelector(".tc-table").classList.remove("d-none")
+        }
+    }
+
     render() {
         // clear the timer that will be set in this method. If it is not set (timer == null), clearTimeout does nothing
         clearTimeout(self.renderTimer)
+
+        if(this.error !== null) {
+            this.showErrorBox(true)
+            this.shadow.querySelector(".tc-error").textContent = this.error.message
+            return
+        }
+
+        this.showErrorBox(false)
 
         const timeDiff = ((this.nextUpdate !== null ? this.nextUpdate : this.lastUpdate) - Date.now()) / 1000
         const reltimefmt = new Intl.RelativeTimeFormat("fr", {numeric: "auto"})
@@ -93,16 +122,71 @@ export default class TrainCard extends HTMLElement {
         this.shadow.querySelector(".tc-train-number").textContent = this.trainNumber
         this.shadow.querySelector(".tc-station").textContent = this.station
         this.shadow.querySelector(".tc-update").textContent = updateText
+
+        const tcRows = this.shadow.querySelector(".tc-rows")
+        const rowTemplate = document.getElementById("train-card-row-template")
+        tcRows.innerHTML = ""
+        this.stops.forEach(stop => {
+            const row = rowTemplate.content.cloneNode(true).firstElementChild // required to edit the reference to the element
+            tcRows.appendChild(row)
+
+            row.querySelector(".tsr-arrival-time").textContent = formatTime(stop.arrivalTime)
+            if(stop.arrivalCanceled)
+                row.querySelector(".tsr-arrival-time").classList.add("canceled")
+            row.querySelector(".tsr-arrival-delay").textContent = formatDelay(stop.arrivalDelay)
+            row.querySelector(".tsr-arrival-delay").classList.add(stop.arrivalDelay > 0 ? "text-danger" : "text-success")
+            
+            row.querySelector(".tsr-departure-time").textContent = formatTime(stop.departureTime)
+            if(stop.departureCanceled)
+                row.querySelector(".tsr-departure-time").classList.add("canceled")
+            row.querySelector(".tsr-departure-delay").textContent = formatDelay(stop.departureDelay)
+            row.querySelector(".tsr-departure-delay").classList.add(stop.departureDelay > 0 ? "text-danger" : "text-success")
+            
+            row.querySelector(".tsr-station").textContent = stop.station
+
+            row.querySelector(".tsr-platform").textContent = stop.platform
+            if(stop.platformChanged)
+                row.querySelector(".tsr-platform").classList.add("text-danger")
+        })
     }
 
     update() {
         clearTimeout(self.UpdateTimer)
 
+        this.stops = []
+        this.error = null
         getVehicle(this.trainNumber)
         .then(res => res.json())
         .then(data => {
+            data.stops.stop.forEach(stop => {
+                this.stops.push({
+                    station: stop.station,
+                    arrivalTime: tsToDate(stop.scheduledArrivalTime),
+                    arrivalDelay: parseInt(stop.arrivalDelay) / 60,
+                    arrivalCanceled: stop.arrivalCanceled == "1",
+                    departureTime: tsToDate(stop.scheduledDepartureTime),
+                    departureDelay: parseInt(stop.departureDelay) / 60,
+                    departureCanceled: stop.departureCanceled == "1",
+                    canceled: stop.canceled == "1",
+                    platform: stop.platform,
+                    platformChanged: stop.platforminfo.normal == "0"
+                })
+            })
+
             this.lastUpdate = new Date()
             this.nextUpdate = null
+        })
+        .catch(err => {
+            if(err.cause?.status == 404)
+                this.error = {
+                    status: 404,
+                    message: "Ce train ne semble pas circuler aujourd'hui."
+                }
+            else
+                this.error = {
+                    status: err.cause?.status,
+                    message: "Une erreur s'est produite."
+                }
         })
         .finally(() => this.render())
     }
